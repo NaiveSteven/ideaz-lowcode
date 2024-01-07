e7a5319698b551043d7d8a648d410f6a58e6390d
NaiveSteven <1004537433@qq.com>
2024-01-07 21:00:39 +0800

    feat: optimize eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,14 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu(
   {
+    stylistic: true,
     unocss: true,
-    formatters: true,
+    typescript: true,
+    vue: true,
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
   },
 )
