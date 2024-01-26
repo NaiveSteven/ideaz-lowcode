71cebc4125c650bedb5803f8c19260bc7a406d13
NaiveSteven <1004537433@qq.com>
2024-01-27 01:05:09 +0800

    refactor: optimize playground component

--- packages/playground-components/src/index.ts
+++ packages/playground-components/src/index.ts
@@ -1,17 +1,18 @@
-import PolyInput from './poly-input';
-import ValueInput from './value-input';
-import PlaceholderBlock from './placeholder-block';
-import ButtonsConfig from './buttons-config';
-import type { App } from 'vue';
+import type { App } from 'vue'
+import ButtonsConfig from './buttons-config'
+import OptionItemWidget from './option-item-widget'
+import PlaceholderBlock from './placeholder-block'
+import PolyInput from './poly-input'
+import ValueInput from './value-input'
 
-const components = [PolyInput, ValueInput, PlaceholderBlock, ButtonsConfig];
+const components = [PolyInput, ValueInput, PlaceholderBlock, ButtonsConfig, OptionItemWidget]
 
-const componentNames = ['PolyInput', 'ValueInput', 'PlaceholderBlock', 'ButtonsConfig'];
+const componentNames = ['PolyInput', 'ValueInput', 'PlaceholderBlock', 'ButtonsConfig', 'OptionItemWidget']
 
-const install = (app: App) => {
+function install(app: App) {
   components.forEach((component, index) => {
-    app.component(componentNames[index], component);
-  });
-};
+    app.component(componentNames[index], component)
+  })
+}
 
-export default { install };
+export default { install }
--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,21 +1,20 @@
-import { createApp } from 'vue'
-import ElementPlus from 'element-plus'
 import * as ElIconModules from '@element-plus/icons-vue'
-import { store } from '@ideal-schema/playground-store'
-import CommonComponents from '@ideal-schema/playground-components'
 import idealUiV3 from '@ideal-schema/ideal-ui-v3'
+import '@ideal-schema/ideal-ui-v3/theme-chalk/src/index.scss'
+import CommonComponents from '@ideal-schema/playground-components'
+import { store } from '@ideal-schema/playground-store'
 import IdeazElement from '@ideaz/element/element'
 import zhCn from '@ideaz/element/locale/lang/zh-cn'
-import App from './App.vue'
-import 'element-plus/dist/index.css'
-import './design/index.scss'
+import '@ideaz/element/theme-chalk/src/index.scss'
 import 'codemirror/mode/javascript/javascript.js'
 import 'codemirror/theme/dracula.css'
-import '@ideal-schema/ideal-ui-v3/theme-chalk/src/index.scss'
-import router from './router/index'
-import '@ideaz/element/theme-chalk/src/index.scss'
-import OptionItemWidgetUse from './widgets/option-item-widget'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import 'uno.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import './design/index.scss'
+import router from './router/index'
 
 function transElIconName(iconName: string) {
   return `i${iconName.replace(/[A-Z]/g, match => `-${match.toLowerCase()}`)}`
@@ -27,8 +26,6 @@ Object.keys(ElIconModules).forEach((item: string) => {
   const cur = item as keyof typeof ElIconModules
   app.component(transElIconName(item), ElIconModules[cur])
 })
-
-app.component(OptionItemWidgetUse.name, OptionItemWidgetUse)
 
 app.use(idealUiV3)
 
