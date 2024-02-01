f6cf8fe26831cc49e7a13f83fd091c519b1a1519
NaiveSteven <1004537433@qq.com>
2024-02-01 21:28:23 +0800

    feat: playground pad simulator supports draggable and optimize style

--- packages/playground/src/widgets/simulator-widget/PadSimulatorWidget.tsx
+++ packages/playground/src/widgets/simulator-widget/PadSimulatorWidget.tsx
@@ -4,10 +4,33 @@ export default defineComponent({
   name: 'PadSimulatorWidget',
   setup() {
     const slots = useSlots()
+    const width = ref(0)
+    const height = ref(0)
+    const isLoading = ref(true)
+
+    onMounted(() => {
+      const dom = document.getElementsByClassName('board')[0]
+      const domRect = dom.getBoundingClientRect()
+      width.value = domRect.width
+      height.value = domRect.height
+      isLoading.value = false
+    })
 
     return () => (
       <div class="board">
-        <drag-resize isActive={true}>{slots.default && slots.default()}</drag-resize>
+        {!isLoading.value && (
+          <drag-resize
+            isActive={true}
+            class="pad-simulator"
+            isDraggable={false}
+            w={width.value}
+            h={height.value}
+          >
+            <div class="h-full w-full" onMousedown={(e: MouseEvent) => { e.stopPropagation() }} onMouseup={(e: MouseEvent) => { e.stopPropagation() }}>
+              {slots.default && slots.default()}
+            </div>
+          </drag-resize>
+        )}
       </div>
     )
   },
