3802b5cc75d47041f303a17678a3d960d688e146
NaiveSteven <1004537433@qq.com>
2024-02-01 21:30:43 +0800

    fix: fix pad simulator drag issue

--- packages/playground/src/widgets/simulator-widget/PadSimulatorWidget.tsx
+++ packages/playground/src/widgets/simulator-widget/PadSimulatorWidget.tsx
@@ -1,12 +1,28 @@
+import { useWorkspaceComponent } from '@ideal-schema/playground-store'
 import './style.scss'
 
 export default defineComponent({
   name: 'PadSimulatorWidget',
   setup() {
     const slots = useSlots()
+    const { curOperateComponent } = useWorkspaceComponent()
+
     const width = ref(0)
     const height = ref(0)
+    const y = ref(0)
+    const x = ref(0)
     const isLoading = ref(true)
+    const dragKey = ref(new Date().valueOf())
+
+    watch(() => curOperateComponent.value, () => {
+      const dom = document.getElementsByClassName('pad-simulator')[0]
+      const domRect = dom.getBoundingClientRect()
+      width.value = domRect.width
+      height.value = domRect.height
+      y.value = domRect.top - 83
+      x.value = domRect.left - 354
+      dragKey.value = new Date().valueOf()
+    })
 
     onMounted(() => {
       const dom = document.getElementsByClassName('board')[0]
@@ -23,8 +39,11 @@ export default defineComponent({
             isActive={true}
             class="pad-simulator"
             isDraggable={false}
+            key={dragKey.value}
             w={width.value}
             h={height.value}
+            y={y.value}
+            x={x.value}
           >
             <div class="h-full w-full" onMousedown={(e: MouseEvent) => { e.stopPropagation() }} onMouseup={(e: MouseEvent) => { e.stopPropagation() }}>
               {slots.default && slots.default()}
