1bd03c9390130292d2c74053676526d853a415cf
NaiveSteven <1004537433@qq.com>
2024-07-22 01:24:11 +0800

    feat: optimize WorkspacePanel component code

--- packages/playground/src/containers/index.ts
+++ packages/playground/src/containers/index.ts
@@ -1,7 +1,7 @@
-import WorkspacePanel from './workspace/WorkspacePanel.vue';
+import WorkspacePanel from './workspace/WorkspacePanel'
 
-export * from './Drawer';
-export * from './Header';
-export * from './composite';
-export * from './Settings';
-export { WorkspacePanel };
+export * from './Drawer'
+export * from './Header'
+export * from './composite'
+export * from './Settings'
+export { WorkspacePanel }
---
+++ packages/playground/src/containers/workspace/WorkspacePanel.tsx
@@ -0,0 +1,58 @@
+import { ViewPort } from '@ideal-schema/playground-demi'
+import { useGlobalSetting, useWorkspaceComponent } from '@ideal-schema/playground-store'
+import { useAsideToggle } from '../../hooks'
+import Selection from '../../widgets/aux-tool-widget/Selection'
+import DesignToolsWidget from '../../widgets/design-tools-widget'
+import MobileSimulatorWidget from '../../widgets/simulator-widget/MobileSimulatorWidget'
+import PadSimulatorWidget from '../../widgets/simulator-widget/PadSimulatorWidget'
+import PcSimulatorWidget from '../../widgets/simulator-widget/PcSimulatorWidget'
+import ViewWidget from '../../widgets/view-widget'
+
+export default defineComponent({
+  name: 'WorkspacePanel',
+  setup() {
+    const { compositeArrowDirection, settingArrowDirection } = useGlobalSetting()
+    const { clickAsideToggleWidget: clickSettingAsideToggle } = useAsideToggle('right', '300px', 300)
+    const { clickAsideToggleWidget: clickCompositeAsideToggle } = useAsideToggle('left', '300px', 300)
+    const { viewType, updateViewType, simulatorType, updateSimulatorType } = useWorkspaceComponent()
+
+    function handleClickView(value: 'json' | 'design' | 'play') {
+      updateViewType(value)
+      const compositeDirection = value === 'design' ? 'left' : 'right'
+      const settingDirection = value === 'design' ? 'right' : 'left'
+      if (compositeArrowDirection.value !== compositeDirection)
+        clickCompositeAsideToggle()
+      if (settingArrowDirection.value !== settingDirection)
+        clickSettingAsideToggle()
+    }
+
+    const simulatorWidget = computed(() => {
+      switch (simulatorType.value) {
+        case 'pc':
+          return PcSimulatorWidget
+        case 'mobile':
+          return MobileSimulatorWidget
+        case 'pad':
+          return PadSimulatorWidget
+      }
+    })
+
+    return () => {
+      return (
+        <div class="workspace-panel">
+          <DesignToolsWidget
+            pixel-type={simulatorType.value}
+            view-type={viewType.value}
+            onClick-pixel={(value: 'pc' | 'mobile' | 'pad') => updateSimulatorType(value)}
+            onClick-view={handleClickView}
+          />
+          <simulatorWidget.value>
+            {viewType.value === 'design' && <Selection />}
+            {viewType.value === 'design' && <ViewPort />}
+            {viewType.value !== 'design' && <ViewWidget view-type={viewType.value} />}
+          </simulatorWidget.value>
+        </div>
+      )
+    }
+  },
+})
