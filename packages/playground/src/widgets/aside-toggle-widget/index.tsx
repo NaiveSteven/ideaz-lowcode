e8431c042ce817591192bc450e56c41abf0080b0
NaiveSteven <1004537433@qq.com>
2024-10-05 23:58:26 +0800

    feat: optimize icon

--- packages/playground/src/widgets/aside-toggle-widget/index.tsx
+++ packages/playground/src/widgets/aside-toggle-widget/index.tsx
@@ -1,5 +1,5 @@
-import { CSSProperties } from 'vue';
-import './style.scss';
+import type { CSSProperties } from 'vue'
+import './style.scss'
 
 export default defineComponent({
   name: 'AsideToggleWidget',
@@ -20,23 +20,23 @@ export default defineComponent({
   emits: ['click'],
   setup(props, { emit }) {
     const style = computed<CSSProperties>(() => {
-      const obj: CSSProperties = {};
-      if (props.left) {
-        obj.left = props.left;
-      }
-      if (props.right) {
-        obj.right = props.right;
-      }
-      return obj;
-    });
+      const obj: CSSProperties = {}
+      if (props.left)
+        obj.left = props.left
+
+      if (props.right)
+        obj.right = props.right
+
+      return obj
+    })
 
     return () => (
       <div class="aside-toggle" style={style.value} onClick={() => emit('click')}>
         <el-icon>
-          {props.arrowDirection === 'left' && <i-arrow-left />}
-          {props.arrowDirection === 'right' && <i-arrow-right />}
+          {props.arrowDirection === 'left' && <i class={['icon-zhixiang-zhishiqizuo', 'iconfont']}></i>}
+          {props.arrowDirection === 'right' && <i class={['icon-zhixiang-zhishiqiyou', 'iconfont']}></i>}
         </el-icon>
       </div>
-    );
+    )
   },
-});
+})
