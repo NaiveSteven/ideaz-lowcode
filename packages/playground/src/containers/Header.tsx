7b2a7017fd6521fdb2d3dc58d39efdb45393034a
NaiveSteven <1004537433@qq.com>
2024-10-09 23:41:55 +0800

    feat: add github button

--- packages/playground/src/containers/Header.tsx
+++ packages/playground/src/containers/Header.tsx
@@ -3,12 +3,27 @@ import './style.scss'
 export const Header = defineComponent({
   name: 'Header',
   setup() {
+    const handleGithub = () => {
+      window.open('')
+    }
+
+    const handleIdeaz = () => {
+      window.open('https://naivesteven.github.io/ideaz-element/')
+    }
+
     return () => (
       <div class="header">
         <div class="logo">
         </div>
         <div class="header--actions">
-          <notify />
+          <el-button size="default" class="mr-4" onClick={handleIdeaz}>
+            <span class="text">Ideaz Element</span>
+          </el-button>
+          <el-button size="default" onClick={handleGithub}>
+            <el-icon size="14"><i class={['icon-github', 'iconfont']}></i></el-icon>
+            <span class="text">Github</span>
+          </el-button>
+          {/* <notify /> */}
         </div>
       </div>
     )
