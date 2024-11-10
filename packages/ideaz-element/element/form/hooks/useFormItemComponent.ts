fbaffd6341fc868521e0d17d62c6b448d3ab7570
NaiveSteven <1004537433@qq.com>
2024-11-10 23:37:48 +0800

    feat: optimize useFormItemComponent hook

--- packages/ideaz-element/element/form/hooks/useFormItemComponent.ts
+++ packages/ideaz-element/element/form/hooks/useFormItemComponent.ts
@@ -1,24 +1,16 @@
 import type { FormItemProps } from '../src/props'
 
 const Z_COMPONENT_NAMES = ['select', 'radio', 'checkbox', 'input']
-const ELEMENT_COMPONENT_NAMES = ['datepicker', 'switch']
 
 export function useFormItemComponent(props: FormItemProps) {
   const getComponentName = (component: string | (() => string)) => {
     const propComponentName = typeof component === 'function' ? component() : component
 
-    if (Z_COMPONENT_NAMES.includes(propComponentName)) {
+    if (Z_COMPONENT_NAMES.includes(propComponentName))
       return `z-${propComponentName}`
-    }
-    else if (ELEMENT_COMPONENT_NAMES.includes(propComponentName)) {
-      if (propComponentName === 'datepicker')
-        return 'el-date-picker'
 
-      return `el-${propComponentName}`
-    }
-    else {
+    else
       return propComponentName || 'unknown'
-    }
   }
 
   const componentName = computed(() => {
